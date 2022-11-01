6dbf029295dc95f4f2a3ffa019f806926768b9fc
MontanoDiego <diego.e2.montano@gmail.com>
2022-11-01 11:02:23 -0700

    add function to change image on click, add image assets

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 /* Imports */
 
 /* Get DOM Elements */
+const oneButton = document.getElementById('one-doom');
+const twoButton = document.getElementById('two-doom');
+const threeButton = document.getElementById('three-doom');
+
+const containerOne = document.getElementById('container-one');
+const containerTwo = document.getElementById('container-two');
+const containerThree = document.getElementById('container-three');
+
+const totalElement = document.getElementById('total');
+const winsElement = document.getElementById('wins');
+const lossElement = document.getElementById('losses');
+
+const beerImage = './assets/one_Beer.png';
+var maskImage = document.getElementById('img-one');
 
 /* State */
+const options = ['one', 'two', 'three'];
 
+
+let oneBeer = 0;
+let totalGuess = 0;
 /* Events */
 
 /* Display Functions */
 
-// (don't forget to call any display functions you want to run on page load!)
+function displayBeer() {
+    console.log('click');
+    maskImage.src = beerImage;
+}
+
+oneButton.addEventListener('click', () => {
+    displayBeer();
+});
+
